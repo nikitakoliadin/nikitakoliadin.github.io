aadd909a97c019259afce6fdb5e32a513d6ec259
Nikita Koliadin <qThegamEp@gmail.com>
2021-01-26 17:28:49 +0200

    Added smooth scroll to top effect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './App.module.css';
 import localization from '../../localization/localization';
 import {ParallaxProvider} from 'react-scroll-parallax';
 import Greeting from '../Greeting/Greeting';
 
 function App() {
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, []);
     return (
         <div className={styles.App}>
             <ParallaxProvider>
