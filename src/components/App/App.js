abbdedcbedfa1e9fcfb1cf8318af9a08f957ec53
Nikita Koliadin <qThegamEp@gmail.com>
2021-01-31 12:16:18 +0200

    Moved parallax provider and parallax cache logic to index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,22 @@
-import React, {useLayoutEffect, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import styles from './App.module.css';
 import localization from '../../localization/localization';
-import {useController, ParallaxProvider} from 'react-scroll-parallax';
 import LinearX from '../LinearX/LinearX';
-
-const ParallaxCache = () => {
-    const {parallaxController} = useController();
-
-    useLayoutEffect(() => {
-        const handler = () => parallaxController.update();
-        window.addEventListener('load', handler);
-        return () => window.removeEventListener('load', handler);
-    }, [parallaxController]);
-
-    return null;
-};
 
 function App() {
     useEffect(() => {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth'
+            behavior: 'smooth',
         });
     }, []);
     return (
         <div className={styles.App}>
-            <ParallaxProvider>
-                <ParallaxCache/>
-                <div className={styles.Greeting}>
-                    <LinearX letters={localization.greeting}
-                             centralize={true}
-                             size={window.innerWidth / 2}/>
-                </div>
-            </ParallaxProvider>
+            <div className={styles.Greeting}>
+                <LinearX letters={localization.greeting}
+                         centralize={true}
+                         size={window.innerWidth / 2}/>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import {useController, ParallaxProvider} from 'react-scroll-parallax';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
 
 console.log(`Version = ${process.env.REACT_APP_VERSION}`);
 console.log(`Register service worker = ${process.env.REACT_APP_REGISTER_SERVICE_WORKER}`);
@@ -15,10 +16,25 @@ if (JSON.parse(process.env.REACT_APP_REGISTER_SERVICE_WORKER)) {
     serviceWorker.unregister();
 }
 
+const ParallaxCache = () => {
+    const {parallaxController} = useController();
+
+    useLayoutEffect(() => {
+        const handler = () => parallaxController.update();
+        window.addEventListener('load', handler);
+        return () => window.removeEventListener('load', handler);
+    }, [parallaxController]);
+
+    return null;
+};
+
 const component = (
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <ParallaxProvider>
+                <ParallaxCache/>
+                <App/>
+            </ParallaxProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
