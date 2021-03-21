bc56e3668ada76662bd569c7ded1656bb75ecba4
Nikita Koliadin <qThegamEp@gmail.com>
2021-03-21 12:08:23 +0200

    Added footer localizations

--- src/localizations/localizations.js
+++ src/localizations/localizations.js
@@ -3,12 +3,27 @@ import LocalizedStrings from 'react-localization';
 let localizations = new LocalizedStrings({
     en: {
         greeting: 'HELLO',
+        footerLinkedIn: 'LinkedIn',
+        footerFacebook: 'Facebook',
+        footerInstagram: 'Instagram',
+        footerTelegram: 'Telegram',
+        footerGitHub: 'GitHub',
     },
     uk: {
         greeting: 'ПРИВІТ',
+        footerLinkedIn: 'LinkedIn',
+        footerFacebook: 'Facebook',
+        footerInstagram: 'Instagram',
+        footerTelegram: 'Telegram',
+        footerGitHub: 'GitHub',
     },
     ru: {
         greeting: 'ПРИВЕТ',
+        footerLinkedIn: 'LinkedIn',
+        footerFacebook: 'Facebook',
+        footerInstagram: 'Instagram',
+        footerTelegram: 'Telegram',
+        footerGitHub: 'GitHub',
     }
 });
 
