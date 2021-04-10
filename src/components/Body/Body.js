ce8d0e31a3a06a37fdfba0a2100da2cc972278bd
Nikita Koliadin <qThegamEp@gmail.com>
2021-04-10 13:25:24 +0300

    Refactored Body component

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -4,52 +4,34 @@ import localizations from '../../localizations/localizations';
 import LinearX from '../LinearX/LinearX';
 
 function Body() {
+    function split(sentence) {
+        return sentence.split(' ').map(word => {
+            return (
+                <div className={styles.Text}>
+                    <LinearX letters={word}
+                             centralize={true}
+                             size={window.innerWidth / 25}/>
+                </div>
+            );
+        });
+    }
+
     return (
         <div className={styles.Body}>
             <div className={styles.Empty}/>
             <div className={styles.Greeting}>
-                <LinearX letters={localizations.greeting}
-                         centralize={true}
-                         size={window.innerWidth / 25}/>
+                {
+                    split(localizations.greeting.toUpperCase())
+                }
             </div>
             <div className={styles.Introducing}>
-                <div className={styles.Me}>
-                    <LinearX letters={localizations.introducingMe}
-                             centralize={true}
-                             size={window.innerWidth / 25}/>
-                </div>
-                <div className={styles.Name}>
-                    <LinearX letters={localizations.introducingName}
-                             centralize={true}
-                             size={window.innerWidth / 25}/>
-                </div>
-                <div className={styles.Surname}>
-                    <LinearX letters={localizations.introducingSurname}
-                             centralize={true}
-                             size={window.innerWidth / 25}/>
-                </div>
+                {
+                    split(localizations.introducing.toUpperCase())
+                }
             </div>
             <div className={styles.Activity}>
-                <div className={styles.And}>
-                    <LinearX letters={localizations.activityAnd}
-                             centralize={true}
-                             size={window.innerWidth / 25}/>
-                </div>
-                <div className={styles.Me}>
-                    <LinearX letters={localizations.activityMe}
-                             centralize={true}
-                             size={window.innerWidth / 25}/>
-                </div>
                 {
-                    localizations.activityPosition.split(' ').map(word => {
-                        return (
-                            <div className={styles.Position}>
-                                <LinearX letters={word}
-                                         centralize={true}
-                                         size={window.innerWidth / 25}/>
-                            </div>
-                        );
-                    })
+                    split(localizations.activity.toUpperCase())
                 }
             </div>
         </div>
